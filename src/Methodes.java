985461bc4a567e4765fc8430aba1c118acbea038
CorinneSpruit <corinnemeijdam@hotmail.com>
2020-10-21 15:05:29 +0200

    Eerste gedeelte

--- src/Methodes.java
+++ src/Methodes.java
@@ -1,2 +1,36 @@
+import java.util.Scanner;
+
 public class Methodes {
+static String gebruiker;
+
+    public static void main (String[] arg){
+
+        helloWorld();
+        groet();
+
+        System.out.println("Wie ben jij?");
+
+        var x  = new Scanner(System.in);
+        var naam = x.nextLine();
+
+        groet(naam);
+
+    }
+
+    private static void groet(String naam) {
+
+        System.out.println("Hallo "+naam);
+
+    }
+
+
+    private static void groet() {
+        System.out.println("Hallo User!");
+    }
+
+    private static void helloWorld() {
+        System.out.println("Hello World!");
+    }
+
+
 }
